2b7df226313aa1ffb7beb2f3a893dcdf812529c5
BRichardev <gui.resende@hotmail.com>
2022-09-22 22:48:12 -0300

    Start is 50% so DO IT!

---
+++ 2 - Elementos Básicos/script.js
@@ -0,0 +1,17 @@
+day = new Date()
+document.body.innerText = `A data e hora atual é exatamente: ${day}`
+
+// Use camelCase
+// Variáveis começam com uma letra minúscula:
+var greenDuck
+
+// Objetos e classes começam com a letra maiúscula:
+var date = Date();
+
+// Contates? são todas maiúscula:
+//const a = CONSTANTLYUPPERCASE;
+
+// Faça seu código bem identado e termine com ponto e virgula
+// Ponto e virgula ajuda na identificação de final de linhas
+
+//! FAÇA COMENTÁRIOS!!
